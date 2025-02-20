96efa94b89d87993e0c8bf04727d0dc3bdc035b7
noone787 <105584563+noone787@users.noreply.github.com>
2025-02-20 13:23:42 +0100

    ZYCIE
    sgz

---
+++ UI/życie.gd
@@ -0,0 +1,23 @@
+extends Control
+
+class_name Zycie
+
+@export var progress_bar: ProgressBar
+
+var max_zycie:int
+var zycie:int
+
+func _ready():
+	max_zycie = 100
+	progress_bar.max_value = max_zycie
+	zycie = 60
+	
+
+func _process(delta: float):
+	progress_bar.value = zycie
+
+func Obrazenia(x):
+	zycie = zycie - x
+	
+func Leczenie(x):
+	zycie = zycie + x
