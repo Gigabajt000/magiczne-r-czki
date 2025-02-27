89f34c5acca0e0704fe6c59319a8560c75627776
noone787 <witcherandmaster@op.pl>
2025-02-27 15:29:24 +0100

    Sklep z życiem zrobione
    Jestes dumny ze nie walnalem poprostu w klawiature?

--- Tile_Map/kupno_życie.gd
+++ Tile_Map/kupno_życie.gd
@@ -4,11 +4,13 @@ extends Area2D
 @export var cena: int
 
 @export var label:Label
+@export var label_cena:Label
 
 func _process(delta: float) -> void:
 	label.text = str(wartosc)
-
+	label_cena.text = str(cena)
+	
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if Global.money >= cena:
+	if Global.money >= cena and Global.życie < Global.max_życie:
 		Global.Obrażenia(-(wartosc))
 		Global.money -= cena
--- global.gd
+++ global.gd
@@ -1,13 +1,13 @@
 extends Node
 
 
-var życie:int = 60
+var życie:int = 20
 var max_życie:int = 100
 
 var stamina:int = 100
 var speed: float
 
-var money:int = 0
+var money:int = 50
 
 func Obrażenia(x):
 	życie -= x
